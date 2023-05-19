6a008b19531244cb64e4d9a44b1fee9235a56a89
Juan Castilla <juan.cas@live.com>
2023-05-19 02:11:43 -0500

    personalizando

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,47 @@
 <template>
-  <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-4" />
+  <v-card width="400">
+      <v-card-item>
+        <v-img
+          
+          src="@/assets/tuca_cover.jpg"
+          cover
+          class="text-white"
+        ></v-img>
+        <!-- <v-card-title>This is a title</v-card-title>
 
-      Base Preset
+        <v-card-subtitle>This is a subtitle</v-card-subtitle> -->
+      </v-card-item>
+
+      <!-- <v-card-text>
+        This is content
+      </v-card-text> -->
+    </v-card>
+
+    <v-card>
+      <v-tabs
+      v-model="tab"
+      bg-color="primary"
+    >
+      <v-tab value="one">Todo</v-tab>
+      <v-tab value="two">Experiencias</v-tab>
+      <v-tab value="three">Coctelería Creativa</v-tab>
+      <v-tab value="three">Coctelería Clásica</v-tab>
+      <v-tab value="three">Shots</v-tab>
+      <v-tab value="three">Piqueos</v-tab>
+    </v-tabs>
+    </v-card>
+  <!-- <v-app-bar flat>
+    <v-app-bar-title>
+      <v-img height="300" src="@/assets/tuca_logo.png" />
     </v-app-bar-title>
-  </v-app-bar>
+  </v-app-bar> -->
 </template>
+<style>
+.v-card-item {
+  padding: 0 !important;
+}
+</style>
 
 <script setup>
-  //
+  //zz
 </script>
