4859f3c5f28133c6e9b7f3891c11b6cc8786bfc3
Juan Castilla <juan.cas@live.com>
2023-05-19 19:58:33 -0500

    version final tuca carta 3

---
+++ assets/Home-56f4ddb0.js
@@ -0,0 +1,1 @@
+import{a7 as c,ac as t}from"./index-61b4c7a8.js";const a={__name:"HelloWorld",setup(e){return(_,r)=>null}},n={__name:"Home",setup(e){return(_,r)=>(c(),t(a))}};export{n as default};
--- assets/Home-a8765eb6.js
+++
@@ -1,1 +0,0 @@
-import{a7 as c,ac as t}from"./index-5ba69ed2.js";const a={__name:"HelloWorld",setup(e){return(_,r)=>null}},n={__name:"Home",setup(e){return(_,r)=>(c(),t(a))}};export{n as default};
