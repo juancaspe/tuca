4573e62638bad094c46207c74a1c4f7150040494
Juan Castilla <juan.cas@live.com>
2023-06-17 16:48:33 -0500

    version final tuca carta 3 actualizado

--- assets/Home-27c472a3.js
+++
@@ -1,1 +0,0 @@
-import{ah as t,an as a}from"./index-9d6f8b48.js";const c={__name:"HelloWorld",setup(e){return(_,r)=>null}},o={__name:"Home",setup(e){return(_,r)=>(t(),a(c))}};export{o as default};
---
+++ assets/Home-d90955ea.js
@@ -0,0 +1,1 @@
+import{ah as t,an as a}from"./index-84c109f9.js";const c={__name:"HelloWorld",setup(e){return(_,r)=>null}},o={__name:"Home",setup(e){return(_,r)=>(t(),a(c))}};export{o as default};
